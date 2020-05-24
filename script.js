757b8ca84a7a739f1bde59c2e49d95aca1a3fa32
monicaaatadeo <monicaaatadeo@gmail.com>
2020-05-24 15:17:32 -0700

    main framework

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log('main.js')
+(function(Jump) {
+    const pointer = document.querySelector(".pointer");
+  
+    pointer.addEventListener("click", e => {
+      Jump(".intro");
+    });
+  })(window.Jump);
