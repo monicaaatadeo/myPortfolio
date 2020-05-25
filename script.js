d882c31ce1e8aa16414509983f919f4d525ad410
monicaaatadeo <monicaaatadeo@gmail.com>
2020-05-24 23:51:52 -0700

    just need to add details

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-console.log('main.js')
-(function(Jump) {
-    const pointer = document.querySelector(".pointer");
-  
-    pointer.addEventListener("click", e => {
-      Jump(".intro");
-    });
-  })(window.Jump);
+console.log('main.js');
